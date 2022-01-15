df92d0c09727bee6400811de0b31c00b0ee5a102
EC2 Default User <ec2-user@ip-172-31-30-202.ec2.internal>
2022-01-15 07:06:50 +0000

    [Upload]5-25,大枠実装完了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,10 +5,24 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @books = @user.books.all
+    @books = @user.books
   end
 
   def edit
     @user = User.find(params[:id])
   end
+  
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path
+  end
+  
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :image, :introduction)
+  end
+  
 end
