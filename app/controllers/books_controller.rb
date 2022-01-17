61d8311fb2eea5bfdd6a4a3fd9207ed1ebc5d795
EC2 Default User <ec2-user@ip-172-31-30-202.ec2.internal>
2022-01-17 06:02:46 +0000

    [Upload]5-25,終了間近

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -21,19 +21,22 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
-    @user = User.find(current_user.id)
+    @user = @book.user
   end
 
   def edit
     @book = Book.find(params[:id])
     @user = User.find(current_user.id)
+    unless @book.user == current_user
+      redirect_to books_path
+    end
   end
 
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
       flash[:notice] = "You have updated book successfully"
-      redirect_to book_path
+      redirect_to book_path(@book.id)
     else
       render:edit
     end
@@ -48,7 +51,7 @@ class BooksController < ApplicationController
   private
 
   def book_params
-    params.require(:book).permit(:title, :opinion)
+    params.require(:book).permit(:title, :body)
   end
 
 end
